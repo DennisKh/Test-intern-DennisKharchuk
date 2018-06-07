77672575ac33e758c4d8c5630ad22536884b775c
DennisKh <40031409+DennisKh@users.noreply.github.com>
2018-06-07 16:59:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let f = (a) => {
+  return func = (b) => {
+    return a + b;
+  };
+}
+console.log(f(2)(3));
---
+++ index2.js
@@ -0,0 +1,40 @@
+let f2 = (inputStr) => {
+  let obj = {};
+  if (inputStr.indexOf('&') != 0 && inputStr.indexOf('.') != 0 && inputStr.indexOf('=') != 0) {
+    inputStr = inputStr.replace(/\"|\"/g,'');
+    inputStr.split('&').map((obj1) => {
+      let index = obj1.indexOf('.');
+      if (obj1.split('=')[1] !='') {
+        if (index != -1){
+          obj[obj1.substring(0, index)] = nesting(obj1.substring(index + 1))
+        } else {
+          obj[obj1.split('=')[0]] = obj1.split('=')[1];
+        }
+      } else {
+        return obj1.split('=')[0] = '';
+      }
+    })
+    console.log(obj);
+  } else {
+    console.log('error');
+  }
+}
+
+let nesting = (str) => {
+  let obj = {};
+  let temp = obj;
+  let object = str.split('=')[0].split('.');
+  let argument = str.split('=')[1];
+  object.map((el, i) => {
+    if (i != object.length -1) {
+      temp = temp[el] = {};
+    } else {
+      temp = temp[el] = argument;
+    }
+    return temp;
+  })
+  return obj;
+}
+f2('this.is.my.first.app="12"');
+f2('user.name=Alex&user.name=Oleg')
+f2('foo.bar27=194d5a7e083f4d3241bd23e2dc2a1e18&boo.foo="10"');
